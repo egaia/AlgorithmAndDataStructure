66ac15eec8c4ee09a0461d512709e7052c443849
Egert Aia <aiaegert@gmail.com>
2016-08-30 17:41:01 +0300

    Added java.util sort, which is too slow.

--- home1/src/Balls.java
+++ home1/src/Balls.java
@@ -1,14 +1,25 @@
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Balls {
 
-   enum Color {green, red};
-   
+   enum Color {green, red}
+
    public static void main (String[] param) {
       // for debugging
    }
    
-   public static void reorder (Color[] balls) {
-      // TODO!!! Your program here
+   static void reorder (Color[] balls) {
+      arraySort(balls); //Speed is 118 ms thus too slow
    }
+
+   /** Sorting array of balls
+    * @param balls Array of balls to be sorted
+    */
+   private static void arraySort(Color[] balls){
+      Arrays.sort(balls, Collections.reverseOrder());
+   }
+
+
 }
 
