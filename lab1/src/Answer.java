431277677b4cd6d325e19527e9dace45d4e5b8ea
Egert Aia <aiaegert@gmail.com>
2016-08-30 17:02:42 +0300

    Finished lab1 tasks

--- lab1/src/Answer.java
+++ lab1/src/Answer.java
@@ -4,18 +4,28 @@ public class Answer {
 
    public static void main (String[] param) {
 
-      // TODO!!! Solutions to small problems 
-      //   that do not need an independent method!
-    
       // conversion double -> String
-
+      String s1 = String.valueOf(2016.);
+      System.out.println(s1);
       // conversion String -> int
+      try{
+         int n1 = Integer.parseInt("2016");
+         System.out.println(n1);
+      } catch (NumberFormatException ex){
+         ex.printStackTrace();
+      }
 
       // "hh:mm:ss"
-
+      Calendar calendar = Calendar.getInstance();
+      String time = calendar.get(Calendar.HOUR_OF_DAY) + ":" + calendar.get(Calendar.MINUTE) + ":" + calendar.get(Calendar.SECOND);
+      System.out.println(time);
       // cos 45 deg
+      System.out.println(Math.cos(Math.toRadians(45)));
 
       // table of square roots
+      for (int i = 0; i<=100;i= i+5){
+         System.out.println("Square root of " + i + " is:" + Math.sqrt(i));
+      }
 
       String firstString = "ABcd12";
       String result = reverseCase (firstString);
@@ -28,21 +38,43 @@ public class Answer {
       System.out.println (s + "\t" + String.valueOf (nw));
 
       // pause. COMMENT IT OUT BEFORE JUNIT-TESTING!
+      /*long startTime = System.nanoTime();
+      try{
+         Thread.sleep(3000);
+      }catch(InterruptedException ex){
+         ex.printStackTrace();
+      }
+      long endTime = System.nanoTime();
+      System.out.println(endTime-startTime);*/
 
       final int LSIZE = 100;
-      ArrayList<Integer> randList = new ArrayList<Integer> (LSIZE);
+      ArrayList<Integer> randList = new ArrayList<>(LSIZE);
       Random generaator = new Random();
       for (int i=0; i<LSIZE; i++) {
-         randList.add (new Integer (generaator.nextInt(1000)));
+         randList.add (generaator.nextInt(1000));
       }
 
       // minimal element
+      System.out.println("Minimal element of list is " + minimum(randList));
 
       // HashMap tasks:
       //    create
+      HashMap<String, String> hashMap = new HashMap<>();
+      hashMap.put("I001", "Esimene õppekava");
+      hashMap.put("I002", "Teine õppeaine");
+      hashMap.put("I003", "Kolmas õppeaine");
+      hashMap.put("I004", "Neljas õppeaine");
+      hashMap.put("I005", "Viies õppeaine");
       //    print all keys
+      System.out.println("All the keys for hashmap are");
+      hashMap.keySet().forEach(System.out::println);
+      System.out.println();
       //    remove a key
+      hashMap.remove("I001");
       //    print all pairs
+      for(Map.Entry<String, String> entry : hashMap.entrySet()){
+         System.out.println("K- " + entry.getKey() + "  V- " + entry.getValue());
+      }
 
       System.out.println ("Before reverse:  " + randList);
       reverseList (randList);
@@ -51,15 +83,38 @@ public class Answer {
       System.out.println ("Maximum: " + maximum (randList));
    }
 
+   /** Finding the minimum element.
+    * @param a Collection of Comparable elements
+    * @return minimum element
+    * @throws NoSuchElementException if <code> a </code> is empty.
+    */
+   private static <T extends Object & Comparable<? super T>> T minimum(Collection<? extends T> a) throws NoSuchElementException{
+      if(!a.isEmpty()){
+         T minimum = null;
+         for(T t : a){
+            minimum = (minimum == null || t.compareTo(minimum) < 0) ? t : minimum;
+         }
+         return minimum;
+      }else{
+         throw new NoSuchElementException();
+      }
+   }
+
    /** Finding the maximal element.
     * @param a Collection of Comparable elements
     * @return maximal element.
     * @throws NoSuchElementException if <code> a </code> is empty.
     */
-   static public <T extends Object & Comparable<? super T>>
-         T maximum (Collection<? extends T> a) 
-            throws NoSuchElementException {
-      return null; // TODO!!! Your code here
+   private static <T extends Object & Comparable<? super T>> T maximum(Collection<? extends T> a) throws NoSuchElementException {
+      if (!a.isEmpty()) {
+         T maximum = null;
+         for (T t : a){
+            maximum = (maximum == null || t.compareTo(maximum) > 0) ? t : maximum;
+         }
+         return maximum;
+      }else{
+         throw new NoSuchElementException();
+      }
    }
 
    /** Counting the number of words. Any number of any kind of
@@ -67,23 +122,31 @@ public class Answer {
     * @param text text
     * @return number of words in the text
     */
-   public static int countWords (String text) {
-      return 0; // TODO!!! Your code here
+   private static int countWords(String text) {
+      return new StringTokenizer(text).countTokens();
    }
 
    /** Case-reverse. Upper -> lower AND lower -> upper.
     * @param s string
     * @return processed string
     */
-   public static String reverseCase (String s) {
-      return null; // TODO!!! Your code here
+   private static String reverseCase (String s) {
+      StringBuilder stringBuffer = new StringBuilder();
+      for (char letter : s.toCharArray()) {
+         stringBuffer.append(Character.isUpperCase(letter) ? Character.toLowerCase(letter) : Character.toUpperCase(letter));
+      }
+      return stringBuffer.toString();
    }
 
    /** List reverse. Do not create a new list.
     * @param list list to reverse
     */
-   public static <T extends Object> void reverseList (List<T> list)
+   private static <T> void reverseList (List<T> list)
       throws UnsupportedOperationException {
-         // TODO!!! Your code here
+         if(!list.isEmpty()){
+            Collections.reverse(list);
+         }else{
+            throw new UnsupportedOperationException();
+         }
    }
 }
