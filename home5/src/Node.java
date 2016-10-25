8f6f944ae530312dee294d8d620d073a34f909d5
Egert Aia <aiaegert@gmail.com>
2016-10-25 10:40:25 +0300

    Added home6 files. Also started building home5 (Node) homework.

--- home5/src/Node.java
+++ home5/src/Node.java
@@ -14,13 +14,54 @@ public class Node {
    }
    
    public static Node parsePostfix (String s) {
+      checkForBasicErrors(s);
+      StringTokenizer st = new StringTokenizer(s, "(),", true);
+      while(st.hasMoreTokens()){
+         String token = st.nextToken().trim();
+         if(token.equals('(')){
+            //samm alla
+         }else if( token.equals(')')){
+            // samm üles
+
+         }else if(token.equals(',')){
+            //samal tasemel samm paremale
+         }
+      }
       return null;
    }
 
    public String leftParentheticRepresentation() {
-      return "";
+      StringBuilder str = new StringBuilder();
+      str.append(this.name);
+      if(this.firstChild != null){
+         str.append("(");
+         str.append(this.firstChild.leftParentheticRepresentation());
+         str.append(")");
+      }
+      if(this.nextSibling != null){
+         str.append(",");
+         str.append(this.nextSibling.leftParentheticRepresentation());
+      }
+
+      return str.toString();
    }
 
+   public static void checkForBasicErrors(String s){
+      if(s.length() == 0)
+         throw new RuntimeException("The tree is empty " + s);
+      if(!s.matches("[\\w(),+--/ *]+"))
+         throw new RuntimeException("String contains illegal symbols: " + s );
+      if(s.contains(" "))
+         throw new RuntimeException("There are empty whitespaces in string " + s);
+      if(s.contains(",,"))
+         throw new RuntimeException("String contains double commas " + s);
+      if(s.contains("()"))
+         throw new RuntimeException("String contains empty subtree " + s);
+      if(s.contains("(("))
+         throw new RuntimeException("String contains double brackets " + s);
+      if(s.contains(",") && !s.contains("(") || !s.contains(")"))
+         throw new RuntimeException("String does not contain brackets " + s);
+   }
    public static void main (String[] param) {
       String s = "(B1,C)A";
       Node t = Node.parsePostfix (s);
