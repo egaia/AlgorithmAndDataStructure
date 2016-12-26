e5337b9a4d657d81ed3e680a7eab6ec699b38963
Egert Aia <aiaegert@gmail.com>
2016-12-26 13:24:14 +0200

    -.-

--- home7/tests/PuzzleTest.java
+++ home7/tests/PuzzleTest.java
@@ -8,11 +8,13 @@ public class PuzzleTest {
 
    @Test (timeout=20000)
    public void test1() { 
-      Puzzle.main (new String[]{"YKS", "KAKS", "KOLM"});
-      Puzzle.main (new String[]{"SEND", "MORE", "MONEY"});
-      Puzzle.main(new String[]{"CRACK", "HACK", "ERROR"});
+      //Puzzle.main (new String[]{"YKS", "KAKS", "KOLM"});
+      //Puzzle.main (new String[]{"SEND", "MORE", "MONEY"});
+      //Puzzle.main(new String[]{"CRACK", "HACK", "ERROR"});
 
-      // Puzzle.main (new String[]{"ABCDEFGHIJAB", "ABCDEFGHIJA", "ACEHJBDFGIAC"});
+
+
+      Puzzle.main (new String[]{"ABCDEFGHIJAB", "ABCDEFGHIJA", "ACEHJBDFGIAC"});
       assertTrue ("There are no tests", true);
    }
 
